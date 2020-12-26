18a82dc0ab287d3b8351adeead0720620f8362ad
Patrick McFern <mcferno@gmail.com>
2020-12-25 22:36:11 -0500

    Making a custom client to get around base URL bug in the external lib

---
+++ src/Client/TwitchAuthClient.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Client;
+
+class TwitchAuthClient extends \GuzzleHttp\Client
+{
+    private const BASE_URI = 'https://id.twitch.tv/oauth2/';
+
+    public function __construct(string $clientId, array $config = [])
+    {
+        parent::__construct($config + [
+            'base_uri' => self::BASE_URI,
+            'timeout' => 30,
+            'headers' => ['Client-ID' => $clientId, 'Content-Type' => 'application/json'],
+        ]);
+    }
+}
