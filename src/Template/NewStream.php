c74b9d60420cd13ace3068c6640ee45dbe744571
Patrick McFern <mcferno@gmail.com>
2025-01-09 21:43:05 -0500

    Isolate template generation for new streams

---
+++ src/Template/NewStream.php
@@ -0,0 +1,57 @@
+<?php
+namespace Template;
+
+class NewStream
+{
+	/**
+	 * Prepares the announcement template to use on Slack to announce a new stream going online.
+	 */
+	static function get(
+		/** Configured account */
+		\Model\Streamer $streamer,
+		/** \Twitch\UserProfile Extended profile data */
+		\stdClass $userProfile,
+		/** \Twitch\OnlineStream Game launched */
+		\stdClass $onlineStream
+	): object
+	{
+		$imageUrl = str_replace(["{width}", "{height}"], ["1280", "720"], $onlineStream->thumbnail_url);
+		$title = str_replace(['"'], ["'"], $onlineStream->title);
+		$gameLabel = !empty($onlineStream->game_name) ? " *{$onlineStream->game_name}*." : "";
+		$gameLabelPlain = !empty($onlineStream->game_name) ? " {$onlineStream->game_name}" : "";
+
+		$profileImage = !empty($userProfile) && !empty($userProfile->profile_image_url)
+			? $userProfile->profile_image_url
+			: $imageUrl;
+
+		$leadInUserMention = !$streamer->hasSlackHandle() ? "<{$streamer->getFeedUrl()}|*{$streamer->getName()}*>" : "<@{$streamer->slackUserId}>";
+
+		return json_decode(<<<REQUEST
+{
+    "text": "{$streamer->getName()} started streaming{$gameLabelPlain}",
+    "blocks": [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "{$leadInUserMention} started streaming :crosshair:{$gameLabel}"
+            }
+        },
+        {
+            "type": "section",
+            "block_id": "section567",
+            "text": {
+                "type": "mrkdwn",
+                "text": "><{$streamer->getFeedUrl()}|*{$onlineStream->user_name}*> {$title}\\n><{$streamer->getFeedUrl()}|{$streamer->getFeedUrl()}>"
+            },
+            "accessory": {
+                "type": "image",
+                "image_url": "{$profileImage}",
+                "alt_text": "User profile photo"
+            }
+        }
+    ]
+}
+REQUEST);
+	}
+}
--- src/cronTasks/postMessage.php
+++ src/cronTasks/postMessage.php
@@ -1,4 +1,5 @@
 <?php
+
 use Utils\Logger;
 use Model\Streamer;
 
@@ -6,21 +7,21 @@ use Model\Streamer;
 include(dirname(dirname(__DIR__)) . "/src/bootstrap.php");
 
 $requiredConfigKeys = [
-    "twitchClientId",
-    "twitchClientSecret",
+	"twitchClientId",
+	"twitchClientSecret",
 	"streamers",
 	"slackWebhookUrl"
 ];
 
 // are we missing any configs?
 if (!$config->hasKeys($requiredConfigKeys)) {
-    Logger::write("Config must contain: " . implode(" | ", $requiredConfigKeys) . "\nExiting..");
-    exit(2);
+	Logger::write("Config must contain: " . implode(" | ", $requiredConfigKeys) . "\nExiting..");
+	exit(2);
 }
 
 if (empty($config->get("streamers"))) {
-    Logger::write("Not configured to pull any streamers. Exiting..");
-    exit(0);
+	Logger::write("Not configured to pull any streamers. Exiting..");
+	exit(0);
 }
 
 $clientId = $config->get("twitchClientId");
@@ -50,10 +51,10 @@ if (!empty($streamList) && !empty($streamList->data)) {
 	$client = new GuzzleHttp\Client();
 	$keystore = new Client\PersistentStore();
 
-    foreach ($streamList->data as $onlineStream) {
+	foreach ($streamList->data as $onlineStream) {
 		$existingStream = $keystore->getActiveTwitchStream($onlineStream->user_id);
 		/** @var Streamer $streamer */
-		$streamer = $streamers[$onlineStream->user_login];
+		$streamer = $streamers[$onlineStream->user_name];
 
 		if (empty($streamer)) {
 			Logger::write("Could not find the config-driven profile for {$onlineStream->user_name}. Skipping ..");
@@ -66,47 +67,8 @@ if (!empty($streamList) && !empty($streamList->data)) {
 				print_r($onlineStream);
 			}
 			Logger::write("Announcing {$onlineStream->user_name} to Slack..");
-			$imageUrl = str_replace(["{width}", "{height}"], ["1280", "720"], $onlineStream->thumbnail_url);
-			$title = str_replace(['"'], ["'"], $onlineStream->title);
-			$gameLabel = !empty($onlineStream->game_name) ? " *{$onlineStream->game_name}*." : "";
-			$gameLabelPlain = !empty($onlineStream->game_name) ? " {$onlineStream->game_name}" : "";
 
-			$userProfile = $keystore->getUserProfile($onlineStream->user_id);
-			$profileImage = !empty($userProfile) && !empty($userProfile->profile_image_url)
-				? $userProfile->profile_image_url
-				: $imageUrl;
-
-			$leadInUserMention = !$streamer->hasSlackHandle() ? "<{$streamer->getFeedUrl()}|*{$streamer->getName()}*>" : "<@{$streamer->slackUserId}>";
-
-			$jsonRequest = <<<REQUEST
-{
-	"text": "{$streamer->getName()} started streaming{$gameLabelPlain}",
-	"blocks": [
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "{$leadInUserMention} started streaming :crosshair:{$gameLabel}"
-			}
-		},
-		{
-			"type": "section",
-			"block_id": "section567",
-			"text": {
-				"type": "mrkdwn",
-				"text": "><{$streamer->getFeedUrl()}|*{$onlineStream->user_name}*> {$title}\\n><{$streamer->getFeedUrl()}|{$streamer->getFeedUrl()}>"
-			},
-			"accessory": {
-				"type": "image",
-				"image_url": "{$profileImage}",
-				"alt_text": "User profile photo"
-			}
-		}
-	]
-}
-REQUEST;
-
-			$jsonRequestObj = json_decode($jsonRequest);
+			$jsonRequestObj = Template\NewStream::get($streamer, $keystore->getUserProfile($onlineStream->user_id), $onlineStream);
 
 			if (empty($jsonRequestObj)) {
 				Logger::write("Failed to build a valid JSON message for Slack for {$onlineStream->user_name}. Skipping ..");
@@ -122,10 +84,12 @@ REQUEST;
 				Logger::write("Failed to announce go live for stream {$onlineStream->user_name}");
 			}
 
-		// stream is already running, see if we need to announce a game change
-		} else if(!empty($onlineStream->game_id)
+			// stream is already running, see if we need to announce a game change
+		} else if (
+			!empty($onlineStream->game_id)
 			&& $existingStream->game_id != $onlineStream->game_id
-			&& !empty($onlineStream->game_name)) {
+			&& !empty($onlineStream->game_name)
+		) {
 
 			if ($debug) {
 				print_r($onlineStream);
@@ -168,7 +132,7 @@ REQUEST;
 		if ($result !== true) {
 			Logger::write("Could not save {$onlineStream->user_id} state to Redis.");
 		}
-    }
+	}
 }
 
 // Logger::write("Exiting normally.");
